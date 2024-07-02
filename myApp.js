f745a8be73806f4225ab6ebbb5e36ea4e8a6ddef
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2024-07-02 18:31:30 +0000

    add logic for send static file to browser

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-// console.log('Hello World');
+
 
 
 
