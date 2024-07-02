30a4e1a4e6b20887b488c36ee62e00df0792868d
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2024-07-02 18:55:52 +0000

    add middleware for style

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
