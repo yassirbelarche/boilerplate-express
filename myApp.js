ca9204fb6a9ea0c48da711930e9af03a1ca2a28f
Banda Adban <mahrifabilahi@gmail.com>
2024-07-31 09:40:14 +0000

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -20,7 +20,7 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get("/json", (req, res) =>{
 
-  if(process.env.MESSAGE_STYLE == "uppercase") {
+  if(process.env.MESSAGE_STYLE="uppercase") {
 
     res.json(
       {"message": "HELLO JSON"}
