ebd5d0693073a6d6969afecc40eca563dfab7820
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-11-11 13:35:56 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 let express = require('express');
 let app = express();
+// Define a route for GET requests to the root path '/'
+app.get('/', (req, res) => {
+  // Respond with the string 'Hello Express'
+  res.send('Hello Express');
+});
+
+// Export the app to be used in server.js
+module.exports = app;
 
 
 
