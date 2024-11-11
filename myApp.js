e56c05d8fbb49e8070643a1dcad3b4b6bf5d03be
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-11-11 13:40:25 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
+app.get('/', (req, res) => {
+  // Respond with the string 'Hello Express'
+  res.send('Hello Express');
+});
 
+// Export the app to be used in server.js
+module.exports = app;
 
 
 
