dc7e85c31deca69c7d2e44668a7752443902ed58
Mohd Afsar Hussain <afsarhussain2200@gmail.com>
2024-11-16 18:52:08 +0000

    node express commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,57 @@
 let express = require('express');
 let app = express();
+const bodyParser=require('body-parser');
+require('dotenv').config();
 
-console.log("Hello World" );
+app.use(bodyParser.urlencoded({extended:false}))
+
+const absoluteAssetPath=__dirname+'/public';
+app.use((req,res,next)=>{
+    // method path - ip
+    console.log(req.method+" "+req.path+" "+"-"+" "+req.ip);
+    next();
+})
+app.use('/public',express.static(absoluteAssetPath));
+
+app.get('/now',(req,res,next)=>{
+    req.time=new Date().toString();
+    next();
+},(req,res)=>{
+    res.json({time:req.time});
+})
+
+
+
+app.get('/:word/echo',(req,res)=>{
+    res.json({echo:req.params.word});
+})
+
+app.route('/name')
+.get((req,res)=>{
+    res.json({name:`${req.query.first} ${req.query.last}`});
+
+})
+.post((req,res)=>{
+    res.json({name:`${req.body.first} ${req.body.last}`});
+    console.log('Post Hitted...')    
+})
+
+
+
+app.get('/',(req,res)=>{
+    let absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath);
+})
+
+app.get('/json',(req,res)=>{
+    if (process.env.MESSAGE_STYLE==='uppercase') {
+
+    res.json({message: 'HELLO JSON'});
+    }else{
+        res.json({message: 'Hello json'});
+    }
+    
+})
 
 
 
